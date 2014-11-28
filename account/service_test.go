021fe951271eb37bf5eea7c3be15b05cfe757dcc
Alberto Leal <albertonb@gmail.com>
2014-11-28 19:40:28 -0200

    Move logic to "account" folder.

---
+++ account/service_test.go
@@ -0,0 +1,112 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/tsuru/config"
+	. "gopkg.in/check.v1"
+
+	"github.com/albertoleal/backstage/db"
+	"github.com/albertoleal/backstage/errors"
+)
+
+type S struct{}
+
+var _ = Suite(&S{})
+
+//Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *S) SetUpSuite(c *C) {
+	config.Set("database:url", "127.0.0.1:27017")
+	config.Set("database:name", "backstage_db_test")
+}
+
+func (s *S) TearDownSuite(c *C) {
+	conn, err := db.Conn()
+	c.Assert(err, IsNil)
+	defer conn.Close()
+	config.Unset("database:url")
+	config.Unset("database:name")
+	// conn.Collection("services").Database.DropDatabase()
+}
+
+func (s *S) TestCreateServiceNewService(c *C) {
+	user := User{Username: "alice"}
+	service := Service{Name: "Backstage",
+		Endpoint:  map[string]interface{}{"latest": "http://example.org/api"},
+		Subdomain: "BACKSTAGE",
+	}
+	err := CreateService(&service, &user)
+	defer DeleteService(&service)
+
+	c.Check(service.Subdomain, Equals, "backstage")
+	_, ok := err.(*errors.ValidationError)
+	c.Check(ok, Equals, false)
+}
+
+func (s *S) TestCannotCreateServiceServiceWhenSubdomainAlreadyExists(c *C) {
+	user := User{Username: "alice"}
+	service := Service{Subdomain: "backstage",
+		Endpoint: map[string]interface{}{"latest": "http://example.org/api"},
+	}
+	err := CreateService(&service, &user)
+	defer DeleteService(&service)
+	c.Check(err, IsNil)
+
+	service2 := Service{Subdomain: "backstage",
+		Endpoint: map[string]interface{}{"latest": "http://example.org/api"},
+	}
+	err = CreateService(&service2, &user)
+	c.Check(err, NotNil)
+
+	e, ok := err.(*errors.ValidationError)
+	c.Assert(ok, Equals, true)
+	message := "There is another service with this subdomain."
+	c.Assert(e.Message, Equals, message)
+}
+
+func (s *S) TestCannotCreateServiceAServiceWithoutRequiredFields(c *C) {
+	user := User{Username: "alice"}
+	service := Service{Subdomain: "backstage"}
+	err := CreateService(&service, &user)
+	e := err.(*errors.ValidationError)
+	message := "Endpoint cannot be empty."
+	c.Assert(e.Message, Equals, message)
+
+	service = Service{}
+	err = CreateService(&service, &user)
+	e = err.(*errors.ValidationError)
+	message = "Subdomain cannot be empty."
+	c.Assert(e.Message, Equals, message)
+}
+
+func (s *S) TestDeleteServiceANonExistingService(c *C) {
+	service := Service{Subdomain: "backstage",
+		Endpoint: map[string]interface{}{"latest": "http://example.org/api"},
+	}
+	err := DeleteService(&service)
+
+	e, ok := err.(*errors.ValidationError)
+	c.Assert(ok, Equals, true)
+	message := "Document not found."
+	c.Assert(e.Message, Equals, message)
+}
+
+func (s *S) TestDeleteServiceAnExistingService(c *C) {
+	user := User{Username: "alice"}
+	service := Service{Subdomain: "backstage",
+		Endpoint: map[string]interface{}{"latest": "http://example.org/api"},
+	}
+
+	count, _ := CountService()
+	c.Assert(count, Equals, 0)
+
+	CreateService(&service, &user)
+	count, _ = CountService()
+	c.Assert(count, Equals, 1)
+
+	DeleteService(&service)
+	count, _ = CountService()
+	c.Assert(count, Equals, 0)
+}
---
+++ account/user_test.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/albertoleal/backstage/errors"
+)
+
+func (s *S) TestCreateUser(c *C) {
+	user := User{Name: "Alice", Email: "foo@bar.com", Username: "alice", Password: "123456"}
+	err := CreateUser(&user)
+	defer DeleteUser(&user)
+	c.Assert(err, IsNil)
+}
+
+func (s *S) TestCreateUserWithSameUsername(c *C) {
+	user := User{Name: "Alice", Email: "foo@bar.com", Username: "alice", Password: "123456"}
+	CreateUser(&user)
+	defer DeleteUser(&user)
+
+	user2 := User{Name: "Bob", Email: "bob@bar.com", Username: "alice", Password: "123456"}
+	err := CreateUser(&user2)
+	e := err.(*errors.ValidationError)
+	msg := "Someone already has that username. Could you try another?."
+	c.Assert(e.Message, Equals, msg)
+}
+
+func (s *S) TestCreateUserWithoutRequiredFields(c *C) {
+	user := User{}
+	err := CreateUser(&user)
+
+	e := err.(*errors.ValidationError)
+	msg := "Name/Email/Username/Password cannot be empty."
+	c.Assert(e.Message, Equals, msg)
+}
+
+func (s *S) TestCreateUserShouldMaskThePassword(c *C) {
+	user := User{Name: "Alice", Email: "foo@bar.com", Username: "alice", Password: "123456"}
+	CreateUser(&user)
+	defer DeleteUser(&user)
+
+	foundUser, _ := FindUserByUsername("alice")
+	c.Assert(foundUser.Password, Not(Equals), "123456")
+}
+
+func (s *S) FindUserByUsername(c *C) {
+	user := User{Name: "Alice", Email: "foo@bar.com", Username: "alice", Password: "123456"}
+	CreateUser(&user)
+	defer DeleteUser(&user)
+
+	foundUser, err := FindUserByUsername("alice")
+	c.Assert(err, IsNil)
+	c.Assert(foundUser, NotNil)
+}
+
+func (s *S) TestFindUserWithInvalidUsername(c *C) {
+	user := User{Name: "Alice", Email: "foo@bar.com", Username: "alice", Password: "123456"}
+	CreateUser(&user)
+	defer DeleteUser(&user)
+
+	_, err := FindUserByUsername("bob")
+	e := err.(*errors.ValidationError)
+	msg := "User not found"
+	c.Assert(e.Message, Equals, msg)
+}
