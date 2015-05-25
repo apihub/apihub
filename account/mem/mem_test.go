df7d6d448e31601ba444f94b4925ad872501f75c
Alberto Leal <albertonb@gmail.com>
2015-05-25 08:58:02 -0300

    Simplify suite test.

--- account/mem/mem_test.go
+++ account/mem/mem_test.go
@@ -7,35 +7,7 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-func TestMem(t *testing.T) { TestingT(t) }
-
-type MemSuite struct {
-	suite test.StorageSuite
-}
-
-var _ = Suite(&MemSuite{})
-
-func (s *MemSuite) SetUpTest(c *C) {
-	storage := New()
-	s.suite.Storage = storage
-}
-
-func (s *MemSuite) TestSaveToken(c *C) {
-	s.suite.TestSaveToken(c)
-}
-
-func (s *MemSuite) TestGetToken(c *C) {
-	s.suite.TestGetToken(c)
-}
-
-func (s *MemSuite) TestGetTokenWithNonExistingKey(c *C) {
-	s.suite.TestGetTokenWithNonExistingKey(c)
-}
-
-func (s *MemSuite) TestDeleteToken(c *C) {
-	s.suite.TestDeleteToken(c)
-}
-
-func (s *MemSuite) TestDeleteTokenWithNonExistingKey(c *C) {
-	s.suite.TestDeleteTokenWithNonExistingKey(c)
+func TestMem(t *testing.T) {
+	Suite(&test.StorableSuite{Storage: New()})
+	TestingT(t)
 }
