86dc845c8d7237e0b014357e5c017f4dfabc5fc8
Alberto Leal <albertonb@gmail.com>
2015-06-25 21:43:18 -0300

    Etcd client.

---
+++ db/etcd_test.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"time"
+
+	"github.com/backstage/maestro/errors"
+	. "gopkg.in/check.v1"
+)
+
+func (s *S) TestEtcdBasicOperationsKey(c *C) {
+	err := s.etcd.SetKey("services", `{"name": "Alice"}`, 0)
+	c.Check(err, IsNil)
+
+	k, _ := s.etcd.GetKey("services")
+	c.Assert(k, Equals, `{"name": "Alice"}`)
+
+	err = s.etcd.DeleteKey("services")
+	c.Check(err, IsNil)
+
+	k, err = s.etcd.GetKey("services")
+	c.Assert(k, Equals, "")
+	_, ok := err.(errors.NotFoundError)
+	c.Assert(ok, Equals, true)
+}
